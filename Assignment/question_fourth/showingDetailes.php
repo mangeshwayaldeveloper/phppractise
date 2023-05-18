2066b92d42f8720a17166782527a3e97e4fdcb54
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-05-19 00:23:08 +0530

    assignment question 4 solved

--- Assignment/question_fourth/showingDetailes.php
+++ Assignment/question_fourth/showingDetailes.php
@@ -2,6 +2,8 @@
 $value=$_POST['flyingFrom'];
 $val2=$_POST['flyingTo'];
 $val3=$_POST['date'];
+echo "<h1><marquee onmouseover='this.stop()' onmouseout='this.start()'>Kingfisher Airlines</marquee></h1>";
 echo "<h2>The Flight From </h2> <h3><i>".$value."</i></h3><h2>To</h2>  <h3><i>".$val2."</i></h3><br>";
-echo "<h3><marquee onmouseover='this.stop()' onmouseout='this.start()'>Is Reschedule On </h3></marqee>".$val3;
+echo "<h3>Is Reschedule On </h3>"."<h4>".$val3."</h4>";
+
 ?>
