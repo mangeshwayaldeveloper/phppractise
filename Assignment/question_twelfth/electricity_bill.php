6d334c761c4fd493de5c19df8307ff48f06f57b0
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-05-25 11:25:31 +0530

    electricity bill

---
+++ Assignment/question_twelfth/electricity_bill.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form id='myForm' action="electricity_bill.php" method="post">
+    <label>Enter Numbers of unit</label>
+    <input type="number" name="unit">
+    <input type="submit" name="subt" onsubmit="hideForm()">
+
+</form>
+</body>
+</html>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $units = (int)$_POST['unit'];
+    if ($units <= 50) {
+        $bill = $units * 3.50;
+        echo "Your Electricity bill is " . $bill;
+    } elseif ($units >= 50 && $units < 150) {
+        $bill = (50 * 3.50) + ($units - 50) * 4.00;
+        echo "Your Electricity bill is " . $bill;
+    } elseif ($units > 150 && $units < 250) {
+        $bill = (50 * 3.50) + ($units - 50) * 4.00 + ($units - 100) * 5.20;
+        echo "Your Electricity bill is " . $bill;
+    } elseif ($units > 250) {
+        $bill = (50 * 3.50) + ($units - 50) * 4.00 + ($units - 100) * 5.20 + ($units - 200) * 6.50;
+        echo "Your Electricity bill is " . $bill;
+    }
+}
+?>
